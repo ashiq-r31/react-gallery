6cfc3ea596986a0caa5585da809dcbfd06dbb668
rahmaas <ash.rahman@verizonwireless.com>
2018-09-20 17:54:37 -0500

    added webpack babel and bundle hashing for html script tags

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ 
+      hash: true, 
+      template: './dist/index.html'
+    })
+  ]
+};

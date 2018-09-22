af19aa758b5e670cb9533be834e720367d46a301
Ashiq <ashiq.r31@gmail.com>
2018-09-22 05:35:48 -0500

    make react component render images

--- src/components/PhotoGallery.jsx
+++ src/components/PhotoGallery.jsx
@@ -1,12 +1,31 @@
 
 import React, { Component } from 'react'
 import styles from './PhotoGallery.scss'
+import seriousImg from '../images/serious-pizza.jpg'
+import pieImg from '../images/pie.png'
+import bb8Img from '../images/bb8_1.svg'
 
 export default class PhotoGallery extends Component {
+  constructor() {
+    super()
+    this.state = {
+      images: [
+        { url: seriousImg, caption: 'Back at my favorite spot in Deep Ellum!' },
+        { url: pieImg, caption: 'They are an actual pie restaurant - just saying.' },
+        { url: bb8Img, caption: 'Best Star Wars character since Chewie.' },
+      ]
+    }
+  }
+
+  createImages() {
+    return this.state.images.map(image => <img src={image.url} alt={image.caption} />)
+  }
+
   render() {
     return (
       <div>
         <h2>Photo Gallery</h2>
+        {this.createImages()}
       </div>
     )
   }

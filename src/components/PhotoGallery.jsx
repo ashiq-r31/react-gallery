b1a8ae5b8a5c366710b9ed022976980f016f0b48
Ashiq <ashiq.r31@gmail.com>
2018-09-22 05:18:38 -0500

    react scaffold

---
+++ src/components/PhotoGallery.jsx
@@ -0,0 +1,13 @@
+
+import React, { Component } from 'react'
+import styles from './PhotoGallery.scss'
+
+export default class PhotoGallery extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Photo Gallery</h2>
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
-import styles from './index.scss'
-import seriousIcon from './images/serious-pizza.jpg'
-import graffitiIcon from './images/graffiti.png'
-import bb8Icon from './images/bb8_1.svg'
 
-const serious = document.createElement('img')
-serious.src = seriousIcon
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PhotoGallery from './components/PhotoGallery'
 
-const graffiti = document.createElement('img')
-graffiti.src = graffitiIcon
-
-const bb8 = document.createElement('img')
-bb8.src = bb8Icon
-
-const app = document.getElementById('app')
-
-app.appendChild(serious)
-app.appendChild(graffiti)
-app.appendChild(bb8)
+ReactDOM.render(<PhotoGallery />, document.getElementById('app'))
